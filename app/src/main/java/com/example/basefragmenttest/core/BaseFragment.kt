877f32386a283a0247f8bd037a77841d26ec9687
mohamed <mohamed_gsm@msn.com>
2021-10-30 18:17:02 +0200

    update of dataBinding work and create base toast

--- app/src/main/java/com/example/basefragmenttest/core/BaseFragment.kt
+++ app/src/main/java/com/example/basefragmenttest/core/BaseFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.widget.Toast
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
@@ -12,21 +12,35 @@ import androidx.lifecycle.ViewModelProvider
 
 abstract class BaseFragment< dataBinding : ViewDataBinding , viewModel : ViewModel > : Fragment() {
 
-
     protected lateinit var viewModel    : viewModel
+
     protected lateinit var binding      : dataBinding
 
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater,getFragmentView(),container,false)
-
         viewModel = ViewModelProvider(this).get(getViewModel())
+
+        binding = getFragmentView()
 
         return binding.root
 
 
     }
 
-    abstract fun getFragmentView()  : Int
+    /* ---- create base dataBinding ---- */
+    abstract fun getFragmentView()  : dataBinding
+
+    /* ---- create base viewModel ---- */
     abstract fun getViewModel()     : Class<viewModel>
+
+    /* ---- create short toast ---- */
+    fun getShortToast(message : String){
+        Toast.makeText(requireActivity(),message,Toast.LENGTH_SHORT).show()
+    }
+
+    /* ---- create long toast ---- */
+    fun getLongToast(message : String){
+        Toast.makeText(requireActivity(),message,Toast.LENGTH_LONG).show()
+    }
+
 }
